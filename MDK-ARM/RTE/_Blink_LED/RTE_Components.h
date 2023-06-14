812ecd0638041b085a5e6f6a5b3322e592f25350
HuangZi-zi <rayfilm@foxmail.com>
2023-06-14 16:38:17 +0800

    HAL_Version

---
+++ MDK-ARM/RTE/_Blink_LED/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Blink_LED' 
+ * Target:  'Blink_LED' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
